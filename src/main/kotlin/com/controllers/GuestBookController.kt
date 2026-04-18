207c1dc719d75787bc1495cbc54c76cec67e6806
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-04-18 23:40:40 +0530

    Renders comments with dummy comments

---
+++ src/main/kotlin/com/controllers/GuestBookController.kt
@@ -0,0 +1,18 @@
+package com.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GuestBookController {
+    @GetMapping("/comments", produces = ["application/json"])
+    fun getComments() = """
+        [
+            {
+                "id": 1,
+                "author": "Anonymous",
+                "text": "Hello, world!"
+            }
+        ]
+    """.trimIndent()
+}
---
+++ src/main/resources/static/guest-book.js
@@ -0,0 +1,52 @@
+const fetchComments = () => fetch('/comments').then(response => response.json());
+
+const createCommentRow = (comment) => {
+    const commentRow = document.createElement('tr');
+
+    const autherCell = document.createElement('td');
+    autherCell.textContent = comment.author;
+
+    const commentCell = document.createElement('td');
+    commentCell.textContent = comment.text;
+
+    commentRow.append(autherCell, commentCell);
+
+    return commentRow;
+};
+
+const createTableHeaderCell = (header) => {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = header;
+    return headerCell;
+}
+
+const createHeaders = (...headers) => {
+    const headersRow = document.createElement('tr');
+
+    const headerElements = headers.map(createTableHeaderCell);
+    headersRow.append(...headerElements);
+
+    return headersRow;
+}
+
+const renderComments = (comments, commentsContainer) => {
+    const commentsHeader = document.createElement("thead");
+    commentsHeader.append(createHeaders("Name", "Comment"));
+
+    const commentsTable = document.createElement('table');
+    commentsTable.append(commentsHeader)
+    commentsTable.append(...comments.map(createCommentRow));
+    commentsTable.border = 1;
+
+    commentsContainer.appendChild(commentsTable);
+};
+
+const main = async () => {
+    const comments = await fetchComments();
+    const commentsContainer = document.getElementById('comments-section');
+    console.log(commentsContainer);
+    console.log(comments);
+    renderComments(comments, commentsContainer);
+}
+
+globalThis.onload = main;
